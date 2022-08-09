9177dce8544b0a83cf690d39cec0b613057e3415
Meh494 <110735674+Meh494@users.noreply.github.com>
2022-08-08 22:50:16 -0400

    Create BlueBook - Times Tables.cpp

---
+++ BlueBook - Times Tables.cpp
@@ -0,0 +1,18 @@
+//Thoughts: Really easy, keep your head clear and be careful with formatting.
+//Meh494
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;  
+    for(int i = 1; i<=t; i++){
+        cout<<t<<" X "<<i<<" = "<<i*t<<"\n";
+    }
+    return 0;
+}
+
