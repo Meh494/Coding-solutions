3caf88a4ffd2a4e9f07247eff96368d685b93116
Meh494 <110735674+Meh494@users.noreply.github.com>
2022-09-17 13:44:29 -0400

    Create CCC '22 J1 - Cupcake Party.cpp

---
+++ CCC '22 J1 - Cupcake Party.cpp
@@ -0,0 +1,16 @@
+//Thoughts: Just make a mathematical formula.
+//Meh494
+
+#include <bits/stdc++.h> 
+using namespace std;
+char alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    
+    
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);  
+     int n, k;
+     cin>>n>>k;
+      cout<<n*8+k*3-28;
+     
+}
