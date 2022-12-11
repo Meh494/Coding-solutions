63b51f6c51a176b8d37828f2f469e99c5177c052
Meh494 <110735674+Meh494@users.noreply.github.com>
2022-12-11 14:11:40 -0500

    Create CCC '20 J1 - Dog Treats.cpp

---
+++ CCC '20 J1 - Dog Treats.cpp
@@ -0,0 +1,18 @@
+//Thoughts: Just implementation
+//Meh494
+
+#include <bits/stdc++.h>
+using namespace std;
+char alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    int s, m, l;
+    cin>>s>>m>>l;
+    if((s+ 2*m + 3*l)>=10)cout<<"happy";
+    else{
+        cout<<"sad";
+    }
+}
